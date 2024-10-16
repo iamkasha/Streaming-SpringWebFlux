e6902822c6d844ba3a9e376b5727a7274abb3323
Uday Kumar <udayrk19@gmail.com>
2024-10-16 15:51:34 -0500

    Streaming using Spring webflux

---
+++ src/test/java/org/kasha/springbootwebflux/streamingspringbootwebflux/StreamingWithSpringBootWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kasha.springbootwebflux.streamingspringbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingWithSpringBootWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
